0427733c6d7bdeb355e6dd8f1672fe51ae1b9300
alyssaurbanski23 <831554@mystma.org>
2021-01-22 17:59:01 +0000

    factOrSum try 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def factOrSum(x, y):
+
+    result = 1
+
+    for i in range (1, x = 1):
+        result = result * i
+
+    return result
+
+print(factOrSum(5, sum))
